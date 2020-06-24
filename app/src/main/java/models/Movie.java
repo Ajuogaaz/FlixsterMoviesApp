32f3f4d62b12012c7777425cbb8794ef7ac31271
linus <brianlinus1753@gmail.com>
2020-06-24 10:56:30 -0700

    "Created Model"

---
+++ app/src/main/java/models/Movie.java
@@ -0,0 +1,14 @@
+package models;
+
+import org.json.JSONObject;
+
+public class Movie{
+
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject){
+        posterPath
+    }
+}
