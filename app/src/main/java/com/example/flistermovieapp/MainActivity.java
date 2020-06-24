de122f25c3fef8007bbf589cce51e21c3f9a3a8a
linus <brianlinus1753@gmail.com>
2020-06-24 11:14:50 -0700

    "Concluded"

--- app/src/main/java/com/example/flistermovieapp/MainActivity.java
+++ app/src/main/java/com/example/flistermovieapp/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results"+ results.toString());
                     movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies"+ movies.size());
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit Json exception, e");
 
