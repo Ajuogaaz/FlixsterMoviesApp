15959f486f692b3c70ea53fba649970d20613a17
linus <brianlinus1753@gmail.com>
2020-06-24 10:27:58 -0700

    "Added json auth"

--- app/src/main/java/com/example/flistermovieapp/MainActivity.java
+++ app/src/main/java/com/example/flistermovieapp/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.flistermovieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final String NOW_PLAYING_URL =
+            "https://api.themoviedb.org/3/movie/now_playing?api_key=c622cdaa84b981fece025799de236e2b";
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+
+                JSONObject jsonObject  = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit Json exception, e");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFail");
+            }
+        });
     }
 }
