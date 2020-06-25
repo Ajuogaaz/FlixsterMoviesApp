c6e6a1eaf1747532641260556f1dfef510830da1
linus <brianlinus1753@gmail.com>
2020-06-25 14:47:02 -0700

    Imported Views

--- app/src/main/java/adapters/MovieDetailsActivity.java
+++ app/src/main/java/adapters/MovieDetailsActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.flistermovieapp.R;
 
@@ -15,6 +18,12 @@ import models.Movie;
 public class MovieDetailsActivity extends AppCompatActivity {
 
     Movie movie;
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+    ImageView ivPoster;
+    TextView Rdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // Unwrap the movie sent via intent
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
 
-        Log.d("MovieDetailsActivity", String.format("Showing Details for %s", movie.getTitle()));
+        //Log.d("MovieDetailsActivity", String.format("Showing Details for %s", movie.getTitle()));
+
+
 
     }
 }
